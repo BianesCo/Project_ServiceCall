14973f59db9c0d5a3caf997cc8002ef4a94fc6ea
BianesCo <bibian.corredor@gmail.com>
2023-08-05 18:32:07 -0500

    subir proyecto

---
+++ src/model/ServicePhone.java
@@ -0,0 +1,37 @@
+package model;
+
+public class ServicePhone {
+	
+	private static final Phone Phone = null;// provicional, Borrar 
+	
+	
+	public void setPhoneOne(Phone phone) {
+		
+	}
+	public void setPhoneTwo(Phone phone) {
+		
+	}
+	public void setPhoneThree(Phone phone) {
+		
+	}
+	
+	public Phone getPhoneOne() {
+		return Phone;
+	}
+	public Phone getPhoneTwo() {
+		return Phone;
+	}
+	public Phone getPhoneThree() {
+		return Phone;
+	}
+	 public boolean registryCall(Phone phone, int age, EtypeCall etypeCall) {
+		 return false;
+	 }
+	 public Phone findPhone(String imai) {
+		 return Phone;
+	 }
+	 public int addMinutes(Phone phone) {
+		 return 0;
+	 }
+
+}
---
+++ src/presenter/HandlingCalls.java
@@ -0,0 +1,28 @@
+package presenter;
+
+import java.time.LocalDate;
+
+import model.ServicePhone;
+
+public class HandlingCalls {
+	
+	private ServicePhone servicePhone;
+
+	public HandlingCalls() {
+		super();
+	}
+	
+	public boolean addPhone(String imai, String number,LocalDate manufacturingDate,String nose, int niIdea ) {
+		return false;
+	}
+	public String findPhone(String nose) {
+		return null;
+	}
+	public int addMinutes(String nose, int niIdea) {
+		return 0;
+	}
+	public boolean registry(String nose,String noIdea,int niIdea) {
+		return false;
+	}
+
+}
